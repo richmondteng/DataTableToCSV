055878edef289e29fd2590b84c00a73937ae2042
DEV-PC3 <DEV-PC3@DEVPC-MON>
2025-02-22 11:43:39 +0800

    prompt message update

--- DataTableToCSV/Form1.cs
+++ DataTableToCSV/Form1.cs
@@ -18,7 +18,7 @@ namespace DataTableToCSV
             callSP2();
             GetCSV2();
             button1.Text = "GENERATE CSV";
-            MessageBox.Show("ok Done!");
+            MessageBox.Show("CSV Generated Successfully!");
 
         }
 
